471cce6fe5cc84400d23faabe89b08ad4e3e0395
Louis Bergelson <louisb@broadinstitute.org>
2018-11-08 16:05:45 -0500

    updating cromshell.rb to remove errors found by audit

--- Formula/cromshell.rb
+++ Formula/cromshell.rb
@@ -1,5 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 class Cromshell < Formula
   desc "Shell commands to interact with a Cromwell server"
   homepage "https://github.com/broadinstitute/cromshell"
@@ -9,24 +7,15 @@ class Cromshell < Formula
 
   bottle :unneeded
 
-  depends_on "jq"
   depends_on "curl"
-  depends_on "cromwell" => [:test, :optional]
+  depends_on "cromwell" => [:optional]
+  depends_on "jq"
 
   def install
-	bin.install "cromshell"
+    bin.install "cromshell"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test cromshell`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "cromshell", "--help"
   end
 end
