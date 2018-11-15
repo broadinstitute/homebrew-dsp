2d57c376a07c6d45d34fbb4535654f461496b4eb
Louis Bergelson <louisb@broadinstitute.org>
2018-11-15 14:39:44 -0500

    Adding formula for japi-compliance-checker
    This is a tool for comparing two different jars of the same library and
    determining if they are binary and source compatible.

---
+++ Formula/japi-compliance-checker.rb
@@ -0,0 +1,18 @@
+class JapiComplianceChecker < Formula
+  desc "Check backwards compatibility of a Java library"
+  homepage "https://lvc.github.io/japi-compliance-checker/"
+  url "https://github.com/lvc/japi-compliance-checker/archive/2.4.tar.gz"
+  sha256 "0fd8ff8539a6f4a2c30379999befc1f9003fbb513f778b018a722360ab8c2229"
+
+  depends_on :java => "1.8+"
+  depends_on "perl"
+
+  def install
+    system "perl", "Makefile.pl", "-install", "-prefix", prefix
+    File.delete "#{bin}/japi-compliance-checker.cmd"
+  end
+
+  test do
+    assert_match "Java API Compliance Checker (JAPICC)", shell_output("#{bin}/japi-compliance-checker --version")
+  end
+end
