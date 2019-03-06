4d4d5bf55615e89545e0e4a1fed97404499eb362
Louis Bergelson <louisb@broadinstitute.org>
2019-03-06 15:08:20 -0500

    updating cromshell to 0.3.3

--- Formula/cromshell.rb
+++ Formula/cromshell.rb
@@ -1,8 +1,8 @@
 class Cromshell < Formula
   desc "Shell commands to interact with a Cromwell server"
   homepage "https://github.com/broadinstitute/cromshell"
-  url "https://github.com/broadinstitute/cromshell/archive/0.3.2.tar.gz"
-  sha256 "827860dde8f1374ab8ee1d3f608c4df97325822554fa51472ed2c622ee24b959"
+  url "https://github.com/broadinstitute/cromshell/archive/0.3.3.tar.gz"
+  sha256 "84229f6707bccf2c6291ad1862e3d53b29d87e78f7eb93f1b0504dec4b931765"
 
   bottle :unneeded
 
