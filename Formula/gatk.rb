96736da59c4f823e5b1632699064a90abba9b0d3
Louis Bergelson <louisb@broadinstitute.org>
2019-12-08 17:45:58 -0500

    upgrade gatk to 4.1.4.1

--- Formula/gatk.rb
+++ Formula/gatk.rb
@@ -1,12 +1,14 @@
 class Gatk < Formula
-  desc "Official code repository for GATK versions 4 and up"
+  # cite McKenna_2010: "https://doi.org/10.1101/gr.107524.110"
+  desc "Genome Analysis Toolkit: Variant Discovery in High-Throughput Sequencing"
   homepage "https://software.broadinstitute.org/gatk"
-  url "https://github.com/broadinstitute/gatk/releases/download/4.1.4.0/gatk-4.1.4.0.zip"
-  sha256 "ae54a2b938f704e15ea03d1822b4ce80d9a02108dc3a2b482d80b93edae3d492"
+  url "https://github.com/broadinstitute/gatk/releases/download/4.1.4.1/gatk-4.1.4.1.zip"
+  sha256 "21ae694cfc8b7447381ad5ce62ed4af22e53a228b12495bdcca7df0c73b09cea"
 
   bottle :unneeded
 
   depends_on :java => "1.8"
+  depends_on "python@2" unless OS.mac?
 
   resource "count_reads.bam" do
     url "https://github.com/broadinstitute/gatk/blob/626c88732c02b0fd5f395db20c91bf2784ec54b9/src/test/resources/org/broadinstitute/hellbender/tools/count_reads.bam?raw=true"
@@ -30,12 +32,13 @@ class Gatk < Formula
     See the GATK readme for detailed installation instructions.
        https://github.com/broadinstitute/gatk
 
-    We recommend running the tools with complex python dependencies by using our docker instead of attempting to install them locally.
-    Gatk dockers are availabe on docker hub:
+    The recommended way of running the tools with complex python dependencies is to use the pre-built docker images instead of attempting to install them locally.
+    Gatk dockers are available on docker hub:
        https://hub.docker.com/r/broadinstitute/gatk/tags/
   EOS
   end
   test do
+    assert_match "Usage", shell_output("#{bin}/gatk --help 2>&1")
     testpath.install resource("count_reads.bam")
     assert_equal "Tool returned:\n8", shell_output("#{bin}/gatk CountReads -I count_reads.bam --tmp-dir #{testpath}").strip
   end
