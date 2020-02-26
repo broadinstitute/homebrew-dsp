f8769f00ff2f8ba3d75ca39bc3ef8561bcbde083
Louis Bergelson <louisb@broadinstitute.org>
2020-02-26 13:26:27 -0500

    update comshell to 0.4.1

--- Formula/cromshell.rb
+++ Formula/cromshell.rb
@@ -1,8 +1,8 @@
 class Cromshell < Formula
   desc "Shell commands to interact with a Cromwell server"
   homepage "https://github.com/broadinstitute/cromshell"
-  url "https://github.com/broadinstitute/cromshell/archive/0.4.0.tar.gz"
-  sha256 "23747803dd21850bc879427f7a17fec29c42f8baf69fcbae51357b3030b2bf47"
+  url "https://github.com/broadinstitute/cromshell/archive/0.4.1.tar.gz"
+  sha256 "cb9aa2065e9c56dee6612caea5f9d19247a13c9103a8a359c7cac40329e14415"
 
   bottle :unneeded
 
