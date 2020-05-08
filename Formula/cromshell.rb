7f4c21efe813527d32d94c8d73d28bcb935eb619
Louis Bergelson <louisb@broadinstitute.org>
2020-05-08 16:15:42 -0400

    upgrade cromshell to 0.4.2
    * upgrade cromshell 0.4.1 -> 0.4.2

--- Formula/cromshell.rb
+++ Formula/cromshell.rb
@@ -1,8 +1,8 @@
 class Cromshell < Formula
   desc "Shell commands to interact with a Cromwell server"
   homepage "https://github.com/broadinstitute/cromshell"
-  url "https://github.com/broadinstitute/cromshell/archive/0.4.1.tar.gz"
-  sha256 "cb9aa2065e9c56dee6612caea5f9d19247a13c9103a8a359c7cac40329e14415"
+  url "https://github.com/broadinstitute/cromshell/archive/0.4.2.tar.gz"
+  sha256 "07a08aca07e74e0547d8eb317cded2967349354c1bcb82e8a59791699b29a446"
 
   bottle :unneeded
 
