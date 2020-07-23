4a1066009123674dd756dbebac689dfd7adacbd2
Louis Bergelson <louisb@broadinstitute.org>
2020-07-23 15:55:14 -0400

    fix audit errors in gatk formula

--- Formula/gatk.rb
+++ Formula/gatk.rb
@@ -7,8 +7,7 @@ class Gatk < Formula
   bottle :unneeded
 
   depends_on :java => "1.8"
-
-  uses_from_macos "python@2"
+  depends_on "python@3.8"
 
   resource "count_reads.bam" do
     url "https://github.com/broadinstitute/gatk/blob/626c88732c02b0fd5f395db20c91bf2784ec54b9/src/test/resources/org/broadinstitute/hellbender/tools/count_reads.bam?raw=true"
@@ -25,21 +24,23 @@ class Gatk < Formula
     bin.install_symlink "#{prefix}/dataproc-cluster-ui"
   end
 
-  def caveats; <<~EOS
-    This brew installation does not include the necessary python dependencies to run certain gatk tools.
-    Similarly, it does not install the necessary version of R and its packages for certain plotting functions to work.
+  def caveats
+    <<~EOS
+      This brew installation does not include the necessary python dependencies to run certain gatk tools.
+      Similarly, it does not install the necessary version of R and its packages for certain plotting functions to work.
 
-    See the GATK readme for detailed installation instructions.
-       https://github.com/broadinstitute/gatk
+      See the GATK readme for detailed installation instructions.
+         https://github.com/broadinstitute/gatk
 
-    The recommended way of running the tools with complex python dependencies is to use the pre-built docker images instead of attempting to install them locally.
-    Gatk dockers are available on docker hub:
-       https://hub.docker.com/r/broadinstitute/gatk/tags/
-  EOS
+      The recommended way of running the tools with complex python dependencies is to use the pre-built docker images instead of attempting to install them locally.
+      Gatk dockers are available on docker hub:
+         https://hub.docker.com/r/broadinstitute/gatk/tags/
+    EOS
   end
   test do
     assert_match "Usage", shell_output("#{bin}/gatk --help 2>&1")
     testpath.install resource("count_reads.bam")
-    assert_equal "Tool returned:\n8", shell_output("#{bin}/gatk CountReads -I count_reads.bam --tmp-dir #{testpath}").strip
+    assert_equal "Tool returned:\n8",
+                 shell_output("#{bin}/gatk CountReads -I count_reads.bam --tmp-dir #{testpath}").strip
   end
 end
