fe48d05250217bd4558f44cfe0f974688e02af2b
Louis Bergelson <louisb@broadinstitute.org>
2021-02-22 16:59:55 -0500

    Fix japi-compliance-checker formula to work with newest brew versions
    * This formula used an out of date style of dependency for specifying java which caused it to start to fail validation

--- Formula/japi-compliance-checker.rb
+++ Formula/japi-compliance-checker.rb
@@ -4,7 +4,7 @@ class JapiComplianceChecker < Formula
   url "https://github.com/lvc/japi-compliance-checker/archive/2.4.tar.gz"
   sha256 "0fd8ff8539a6f4a2c30379999befc1f9003fbb513f778b018a722360ab8c2229"
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk@8"
   depends_on "perl"
 
   def install
