3d2de228d82f593dfb2dc3ef8434a1310e1e2999
Louis Bergelson <louisb@broadinstitute.org>
2021-07-28 15:41:08 -0400

    Update cromshell to 0.5.0
    *update cromshell from 0.4.4 -> 0.5.0

--- Formula/cromshell.rb
+++ Formula/cromshell.rb
@@ -1,11 +1,12 @@
 class Cromshell < Formula
   desc "Shell commands to interact with a Cromwell server"
   homepage "https://github.com/broadinstitute/cromshell"
-  url "https://github.com/broadinstitute/cromshell/archive/0.4.4.tar.gz"
-  sha256 "f50ca66529ec6a544866eb0d677da2e2caec6108bb88fc1f8861317d6326d743"
+  url "https://github.com/broadinstitute/cromshell/archive/0.5.0.tar.gz"
+  sha256 "0c4eeeab14524ce847132e714e59d95dd60c4e1a1aaaba3d6ff8df5999f261b1"
 
   bottle :unneeded
 
+  depends_on "coreutils"
   depends_on "jq"
   depends_on "cromwell" => [:optional]
 
