081c48f1a425a0da3ca3d4989d160286efe0fb90
Louis Bergelson <louisb@broadinstitute.org>
2022-10-20 10:41:29 -0400

    update cromshell 0.5.2 -> 0.5.3

--- Formula/cromshell.rb
+++ Formula/cromshell.rb
@@ -1,8 +1,8 @@
 class Cromshell < Formula
   desc "Shell commands to interact with a Cromwell server"
   homepage "https://github.com/broadinstitute/cromshell"
-  url "https://github.com/broadinstitute/cromshell/archive/0.5.2.tar.gz"
-  sha256 "55c2a6181c0d292ba8598090431693933ffa36cf7730300e134857dd1ff73705"
+  url "https://github.com/broadinstitute/cromshell/archive/0.5.3.tar.gz"
+  sha256 "64b9d422330b4bb4759faf0193c740cb35edd508e1180e2def352309e3e29e01"
 
   depends_on "coreutils"
   depends_on "jq"
