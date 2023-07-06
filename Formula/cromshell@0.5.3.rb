60bdfeecb70c44eee7fc630b6b3820c748a5d152
bshifaw <bshifaw@broadinstitute.org>
2023-07-06 20:04:38 +0300

    Updated cromshell formula to 2.0.0 (#3)
    * updated cromshell.rb using 2.0.0, removed alpha and beta versions
    
    * rm eof newline
    
    * add rb for cromshell@0.5.3
    
    ---------
    
    Co-authored-by: bshifaw <bshifaw@broadinstitute.com>

---
+++ Formula/cromshell@0.5.3.rb
@@ -0,0 +1,19 @@
+class Cromshell@053 < Formula
+  desc "Shell commands to interact with a Cromwell server"
+  homepage "https://github.com/broadinstitute/cromshell"
+  url "https://github.com/broadinstitute/cromshell/archive/0.5.3.tar.gz"
+  sha256 "64b9d422330b4bb4759faf0193c740cb35edd508e1180e2def352309e3e29e01"
+
+  depends_on "coreutils"
+  depends_on "jq"
+  depends_on "cromwell" => [:optional]
+
+  def install
+    bin.install "cromshell"
+    zsh_completion.install "completions/_cromshell" => "_cromshell"
+  end
+
+  test do
+    system "#{bin}/cromshell", "--help"
+  end
+end
